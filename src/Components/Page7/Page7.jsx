394accea6ced6ff8fb1d5a4a106d2614efa7b04d
ArunimaGupta1 <arunimagupta71@gmail.com>
2024-04-09 17:29:35 +0530

    second commit

--- src/Components/Page7/Page7.jsx
+++ src/Components/Page7/Page7.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import './Page7.css';
 
 function Page7() {
   return (
     <div>
-      <div className="container">
-        <h1>
-          As a global business, we encourage you to contact us no matter where
+      <div className="container text-center">
+        <h4>
+          As a <div className="color">global business</div>, we encourage you to contact us no matter where
           you are located in the world.
-        </h1>
+        </h4>
         <h6>Browse our job opportunities across the globe.</h6>
       </div>
       <div className="container1">
-        <h4>OPEN ROLES</h4>
-        <img src="./Group 13.png" alt="text" />
+        <h4 className="text-center">OPEN ROLES <img src="./Group 13.png" alt="text"/></h4>
+      </div>
+      <div className="pic">
+        <center><img src="./Group 1585.png" alt="map"/></center>
       </div>
     </div>
   );
